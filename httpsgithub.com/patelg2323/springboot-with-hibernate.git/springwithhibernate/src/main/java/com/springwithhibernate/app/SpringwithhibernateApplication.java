758a9d45994812490c7bce88a64a0ba0d8141c66
patelg2323 <77806834+patelg2323@users.noreply.github.com>
2021-01-22 01:56:18 +0530

    Signed-off-by: patelg2323 <77806834+patelg2323@users.noreply.github.com>

---
+++ httpsgithub.com/patelg2323/springboot-with-hibernate.git/springwithhibernate/src/main/java/com/springwithhibernate/app/SpringwithhibernateApplication.java
@@ -0,0 +1,13 @@
+package com.springwithhibernate.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwithhibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwithhibernateApplication.class, args);
+	}
+
+}
---
+++ httpsgithub.com/patelg2323/springboot-with-hibernate.git/springwithhibernate/src/test/java/com/springwithhibernate/app/SpringwithhibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwithhibernate.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwithhibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
